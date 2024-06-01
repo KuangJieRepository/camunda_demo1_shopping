59032cd8eae0e20e9f09dafb473b3a6b95ce5573
kuangjie <1760141843@qq.com>
2024-06-01 12:32:28 +0800

    first commit

---
+++ src/main/java/com/kj/camunda_demo1/CamundaDemo1Application.java
@@ -0,0 +1,13 @@
+package com.kj.camunda_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kj/camunda_demo1/CamundaDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj.camunda_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
